a4e07fc4a4215087b2737391106bb8290b2bf089
mariamonica <monicamicabalo@gmail.com>
2016-07-19 19:36:20 +0800

    PRELIM

---
+++ newgrade.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function gradeEven(inptgrade) {
+    return (grades%2)==0;
+    }
+function gradeOdd(inptgrade) { 
+    return !gradeEven(grades); 
+    }
+
+rl.question('\nPlease enter your grade: ', (inptgrade) => {
+    
+  if(inptgrade < 75 && inptgrade == gradeEven)
+  console.log('Your new grade is: '+ (inptgrade - 2));
+  rl.close();
+});
+
